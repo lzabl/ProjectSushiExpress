79f603a701e7338ca32e0eb28504f07420389337
lzabl <laura.zabielaite@gmail.com>
2022-03-08 22:49:42 +0200

    Added screenshots

---
+++ Tools/MyScreenshot.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest1.Tools
+{
+    public class MyScreenshot
+    {
+        public static void TakeScreenshot(IWebDriver driver)
+        {
+            Screenshot screenshot = driver.TakeScreenshot();
+
+            string screenshotDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
+            string screenshotFolder = Path.Combine(screenshotDir, "screenshots");
+            Directory.CreateDirectory(screenshotFolder);
+            string screenshotName = $"{TestContext.CurrentContext.Test.Name}_{DateTime.Now:HH_mm_ss}.png";
+            string screenshotPath = Path.Combine(screenshotFolder,screenshotName);
+            screenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+        }
+    }
+}
