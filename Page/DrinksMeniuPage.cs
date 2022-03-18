8c2bd61c25a0b346d19d1b8898ee8b7e9383bcb5
lzabl <100605807+lzabl@users.noreply.github.com>
2022-03-18 12:19:59 +0200

    Update DrinksMeniuPage.cs

--- Page/DrinksMeniuPage.cs
+++ Page/DrinksMeniuPage.cs
@@ -1,10 +1,5 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoTest1.Page
 {
@@ -12,12 +7,14 @@ namespace AutoTest1.Page
     {
         private IWebElement drinkMeniu => Driver.FindElement(By.CssSelector("#menu-item-422 > div > a"));
         private IWebElement drinkKombucha => Driver.FindElement(By.CssSelector("#filtered > div.col-lg-4.col-md-6.col-xs-12.post-51145.product.type-product.status-publish.has-post-thumbnail.product_cat-gerimai.last.instock.taxable.shipping-taxable.purchasable.product-type-simple > div"));
+        private IWebElement GoBackToMeniu => Driver.FindElement(By.CssSelector("body > main > section > div > div > div.back > a"));
+        private IWebElement AddKombuchaToCart => Driver.FindElement(By.CssSelector("#product-51145 > div.main-block.clearfix > div.summary-product.col-md-6.col-sm-6.col-xs-12 > form > button"));
 
         public DrinksMeniuPage(IWebDriver webdriver) : base(webdriver) { }
 
         public void GoBackToFullMeniu()
         {
-            IWebElement GoBackToMeniu = Driver.FindElement(By.CssSelector("body > main > section > div > div > div.back > a"));
+    
             GoBackToMeniu.Click();
         }
         public void OpenDrinksMeniu()
@@ -27,14 +24,12 @@ namespace AutoTest1.Page
         public void AddKombucha(string drinkName)
         {
             drinkKombucha.Click();
-            IWebElement AddKombuchaToCart = Driver.FindElement(By.CssSelector("#product-51145 > div.main-block.clearfix > div.summary-product.col-md-6.col-sm-6.col-xs-12 > form > button"));
+         
             AddKombuchaToCart.Click();
 
             string CartContent = Driver.FindElement(By.CssSelector("body > main > section > div > div > div.woocommerce-notices-wrapper > div")).Text;
 
             Assert.AreEqual(drinkName, CartContent, "Wrong drink added to cart");
         }
-  
     }
-    
 }
