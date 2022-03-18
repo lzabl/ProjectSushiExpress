7ce3488f7417ad802b18b5efa97d4216f0adb1df
lzabl <100605807+lzabl@users.noreply.github.com>
2022-03-18 12:23:51 +0200

    Update SushiBaseTest.cs

--- Test/SushiBaseTest.cs
+++ Test/SushiBaseTest.cs
@@ -1,30 +1,27 @@
-﻿using AutoTest1.Driver;
+using AutoTest1.Driver;
 using AutoTest1.Page;
+using AutoTest1.Tools;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoTest1.Test
 {
     public class SushiBaseTest
-    { 
-    public static IWebDriver driver;
-    public static SushiOrderPage sushiOrderPage;
-    public static SushiMeniuPage sushiMeniuPage;
-    public static DrinksMeniuPage drinksMeniuPage;
+    {
+        public static IWebDriver driver;
+        public static SushiOrderPage sushiOrderPage;
+        public static SushiMeniuPage sushiMeniuPage;
+        public static DrinksMeniuPage drinksMeniuPage;
 
         [OneTimeSetUp]
         public static void SetUp()
-            {
+        {
             driver = CustomDriver.GetChromeDriver();
             sushiOrderPage = new SushiOrderPage(driver);
             sushiMeniuPage = new SushiMeniuPage(driver);
             drinksMeniuPage = new DrinksMeniuPage(driver);
-            }
+        }
         [TearDown]
         public static void TearDown()
         {
@@ -35,9 +32,10 @@ namespace AutoTest1.Test
         }
 
         [OneTimeTearDown]
-        public static void TearDown()
-            {
+        public static void OneTimeTearDown()
+        {
             driver.Quit();
-            }
+        }
+
     }
 }
